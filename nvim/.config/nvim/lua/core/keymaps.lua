ab33327fd047ae581c11dd37519b65c4692361e7
asce4s <asce4s@gmail.com>
2025-08-10 13:31:24 +0800

    random configs

--- nvim/.config/nvim/lua/core/keymaps.lua
+++ nvim/.config/nvim/lua/core/keymaps.lua
@@ -22,5 +22,5 @@ keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  
 keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
 keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
 
-keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Scroll down half page and center cursor" })
 keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Scroll up half page and center cursor" })
+keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Scroll down half page and center cursor" })
